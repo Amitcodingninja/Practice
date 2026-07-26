7756db338345352c370d4d41b3e36256c26d9c8b
Amit <akumar420india@gmail.com>
2026-07-26 12:30:34 +0530

    BH-123/Added Counter button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  return (
-    <div className='flex items-center justify-center h-screen flex-col'>
-        Hello World
-      
-    </div>
-  )
+
+    const [count, setCount] = useState(0);
+
+    function Increase() {
+        setCount(count + 1)
+    }
+    function Decrease() {
+        setCount(count - 1)
+    }
+    function Reset() {
+        setCount(0)
+    }
+    return (
+        <div className='flex items-center justify-center h-screen gap-2'>
+            <div className='flex'>
+                <button
+                    className='border-2 p-2 cursor-pointer rounded-2xl bg-emerald-300'>
+                    {count}
+                </button>
+            </div>
+            <button
+                className='border-2 p-2 cursor-pointer rounded-2xl bg-amber-800'
+                onClick={Increase}
+            >
+                Increase
+            </button>
+
+            <button
+                className='border-2 p-2 cursor-pointer rounded-2xl bg-amber-400'
+                onClick={Decrease}
+            >
+                Decrease
+            </button>
+
+            <button
+                className='border-2 p-2 cursor-pointer rounded-2xl bg-amber-200'
+                onClick={Reset}
+            >
+                Reset
+            </button>
+
+        </div>
+    )
 }
 
 export default App
